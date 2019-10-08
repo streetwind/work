02e559aef764cb013c3992d663ffd8e6b7f2812e
mofeng <mofeng64@gmail.com>
2019-10-08 16:56:24 +0800

    rpio init

--- rpi/main.go
+++ rpi/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main(){}
+import (
+	rpio "github.com/stianeikeland/go-rpio/v4"
+)
+
+func main() {
+	rpio.Open()
+	def rpio.Close()
+	pin := rpio.Pin(10)
+	pin.Output()
+	pin.High()
+}
